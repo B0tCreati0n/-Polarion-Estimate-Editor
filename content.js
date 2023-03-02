c649ae86440734a1eab49fadfeac1c5e1ae31422
B0tCreati0n <noahgajnedergaard@gmail.com>
2023-03-03 00:34:45 +0100

    content will now only run when it is intended to

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
+import { userExtensionActive, extensionActive } from './config.js';
+
+if (userExtensionActive && extensionActive) {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'setCustomLabel' && userExtensionActive && extensionActive) {
       console.log(`Setting custom label to: ${message.customLabel}`);
       document.getElementById("site-name-input").value = message.customLabel;
     }
   });
-  
+}
