8685cb1aa1280cbb09a6488b3b04c34526d6c44d
B0tCreati0n <noahgajnedergaard@gmail.com>
2023-03-02 23:58:29 +0100

    added content.js and edited background.js

--- background.js
+++ background.js
@@ -1,35 +1,30 @@
 let extensionActive = false;
-import {userExtensionActive} from './main.js';
+import { userExtensionActive } from './main.js';
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (changeInfo.status == "complete") {
     console.log(tab.url);
     if (tab.url.indexOf(siteUrl) != -1) {
       extensionActive = true;
-    }
-    else {
+      chrome.tabs.executeScript(tabId, {
+        file: 'content.js'
+      });
+    } else {
       extensionActive = false;
-    }
-  }
-
-  //fetch "siteUrl" and replace with customLabel
-  if (extensionActive && userExtensionActive) {
-    try {
-      const response = await fetch(siteUrl);
-      const html = await response.text();
-      if (html.includes('Rough Estimated (Days)')) {
-        console.log("FOUND Rough Estimated (Days)");
-        document.getElementById("site-name-input").value = customLabel;
-      } else {
-        console.log("did NOT find Rough Estimated (Days)");
-      }
-    } catch (error) {
-      console.error("Error fetching website:", error);
     }
   }
 });
 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === 'setCustomLabel') {
+    console.log(`Setting custom label to: ${message.customLabel}`);
+    chrome.tabs.executeScript({
+      code: `document.getElementById("site-name-input").value = "${message.customLabel}"`
+    });
+  }
+});
+
 //Debug logs
-setInterval(function() {
+setInterval(function () {
   console.log(extensionActive);
 }, 5000);
