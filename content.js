44cbb1cdaa5d8fece5b4cfff8bbc47538d319379
B0tCreati0n <noahgajnedergaard@gmail.com>
2023-03-03 01:03:51 +0100

    fixed not sreaching and replacing text strings

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
-import { userExtensionActive, extensionActive } from './config.js';
+import { userExtensionActive, extensionActive, customLabel } from './config.js';
 
 if (userExtensionActive && extensionActive) {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === 'setCustomLabel' && userExtensionActive && extensionActive) {
       console.log(`Setting custom label to: ${message.customLabel}`);
-      document.getElementById("site-name-input").value = message.customLabel;
+      customLabel = message.customLabel;
+      document.body.innerText = document.body.innerText.replace(/Rough Estimate \(Days\)/gi, customLabel);
     }
   });
 }
